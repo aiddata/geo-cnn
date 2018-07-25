c47f103ff5f9b509ae81b33ded06af02901f0a65
userz <sgoodman@aiddata.org>
2018-07-25 14:03:38 -0400

    Add script for copying files from git repo to sciclone dir

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+src=/home/userz/git/geo-cnn
+dst="/sciclone/aiddata10/REU/projects/mcc_tanzania"
+
+cp $src/create_grid.py $dst
+cp $src/runscript.py $dst
+cp $src/jobscript $dst
+cp $src/resnet.py $dst
