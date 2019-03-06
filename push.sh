bf6634d3af2e2f723169cea2f5d0e08edb46889a
userz <sgoodman@aiddata.wm.edu>
2019-03-06 13:27:03 -0500

    Add prepare survey data script (was in survey data dir on sciclone)

--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@
 src=/home/userz/git/geo-cnn
 dst="/sciclone/aiddata10/REU/projects/mcc_tanzania"
 
+cp $src/prepare_survey_data.py $dst
 cp $src/create_grid.py $dst
 cp $src/runscript.py $dst
 cp $src/jobscript $dst
