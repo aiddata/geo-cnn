13b4d079f21221f4642813ef2ad92847f753abc7
userz <sgoodman@aiddata.wm.edu>
2019-03-21 14:03:42 -0400

    Add survey prep script to push

--- push.sh
+++ push.sh
@@ -4,12 +4,13 @@ src=/home/userz/git/geo-cnn
 dst="/sciclone/aiddata10/REU/projects/mcc_tanzania"
 
 cp $src/prepare_survey_data.py $dst
+cp $src/jobscript $dst
+cp $src/data_prep.py $dst
 cp $src/create_grid.py $dst
 cp $src/runscript.py $dst
-cp $src/jobscript $dst
 cp $src/resnet.py $dst
 cp $src/vgg.py $dst
 cp $src/load_data.py $dst
-cp $src/data_prep.py $dst
+cp $src/load_survey_data.py $dst
 cp $src/main.py $dst
 cp $src/second_stage_model.py $dst
