2f81b2da7d726bd9e5f904a1d1729e7e36031071
userz <sgoodman@aiddata.wm.edu>
2019-03-27 16:57:40 -0400

    Update sampling in data prep (ongoing)

--- data_prep.py
+++ data_prep.py
@@ -42,20 +42,17 @@ base_path = "/sciclone/aiddata10/REU/projects/mcc_tanzania"
 # -----------------------------------------------------------------------------
 
 
-
 class NTL():
     def __init__(self):
         self.base = "/sciclone/aiddata10/REU/geo/data/rasters/dmsp_ntl/v4composites_calibrated_201709"
-
     def set_year(self, year):
         self.year = year
         self.path = glob.glob(os.path.join(self.base, "*{0}*.tif".format(self.year)))[0]
         self.file = rasterio.open(self.path)
-
     def value(self, lon, lat, ntl_dim=7):
         """Get nighttime lights average value for grid around point
         """
-        r, c = ntl_file.index(lon, lat)
+        r, c = self.file.index(lon, lat)
         ntl_win = ((r-ntl_dim/2+1, r+ntl_dim/2+1), (c-ntl_dim/2+1, c+ntl_dim/2+1))
         ntl_data = self.file.read(1, window=ntl_win)
         ntl_mean = ntl_data.mean()
@@ -66,21 +63,9 @@ ntl_year = 2010
 ntl = NTL()
 ntl.set_year(ntl_year)
 
-# -----------------------------------------------------------------------------
-
-
-# lsms_clusters_path = os.path.join(
-#     base_path, "data/surveys/final/tanzania_2012_lsms_cluster.csv")
-
-# lsms_cluster = pd.read_csv(lsms_clusters_path, quotechar='\"',
-#                            na_values='', keep_default_na=False,
-#                            encoding='utf-8')
-
-# lsms_cluster['ntl'] = lsms_cluster.apply(
-#     lambda z: ntl.value(z['lon'], z['lat']), axis=1)
-
 
 # -----------------------------------------------------------------------------
+
 
 print("\nPreparing grid..")
 
@@ -116,50 +101,22 @@ else:
     grid.to_csv(csv_path)
     df = grid.df.copy(deep=True)
 
+# ==============================
+# copy of full df
+
+original_df = df.copy(deep=True)
+
+# for use in debugging
+# df = original_df.copy(deep=True)
+
+
 # -----------------------------------------------------------------------------
 
-# =============================================================================
 
-# consumption_list = list(lsms_cluster['cons'])
-# cat_vals = [np.percentile(consumption_list, x*100/ncats) for x in range(1, ncats+1)]
-
-# def classify(val):
-#     for cix, cval in enumerate(cat_vals):
-#         if val <= cval:
-#             return cat_names[cix]
-#     print(val)
-#     raise Exception("Could not classify")
-
-# lsms_cluster['label'] = lsms_cluster.apply(
-#     lambda z: classify(z['cons']), axis=1)
-
-# class_ntl_means = dict(zip(cat_names, lsms_cluster.groupby('label')['ntl'].mean()))
-
-# class_ntl_means = {
-#     0: 2.0,
-#     1: 5.0,
-#     2: 12.0
-# }
-
-# # find nearest neighbor for each grid cell using class_ntl_means
-
-# # set all where ntl <  lowest class automatically to lowest class
-# df['label'] = None
-# df.loc[df['ntl'] <= class_ntl_means[min(cat_names)], 'label'] = min(cat_names)
+print("\nBuilding datasets...")
 
 
-# def find_nn(val):
-#     nn_options = [(k, abs(val - v)) for k, v in class_ntl_means.iteritems()]
-#     nn_class = sorted(nn_options, key=lambda x: x[1])[0][0]
-#     return nn_class
-
-# # run nearest neigbor class label for remaining
-# to_label = df['ntl'] > class_ntl_means[min(cat_names)]
-# df.loc[to_label, 'label'] = df.loc[to_label].apply(lambda z: find_nn(z['ntl']), axis=1)
-
-# -------
-
-# bin labeling
+# label each point based on ntl value for point and class bins
 
 class_bins = {
     0: [0, 3],
@@ -172,38 +129,65 @@ for c, b in class_bins.iteritems():
     df.loc[df['ntl'] >= b[0], 'label'] = int(c)
 
 
-# =============================================================================
+# ==============================
+
+# determine size of each data type (train, val, etc)
+
+type_names = ["train", "val", "test", "predict"]
+
+# ratio for train, val, test data
+# must sum to 1.0
+type_weights = [0.850, 0.150, 0.0, 0.0]
+
+
+def gen_sample_size(count, weights):
+    type_sizes = np.zeros(len(weights)).astype(int)
+    # note: sizes are based on nkeep which is for a single NTL class label
+    # subsequent steps to label data class (train, val, etc) must be repeated
+    # for each NTL class
+    for i,x in enumerate(weights):
+        type_sizes[i] = int(np.floor(count * x))
+    # used floor for all counts, so total can be short by one
+    active_type_weights_indexes = [i for i,j in enumerate(weights) if j>0]
+    for _ in range(count - sum(type_sizes)):
+        bonus_type_weights_index = active_type_weights_indexes[np.random.randint(0, len(active_type_weights_indexes))]
+        type_sizes[bonus_type_weights_index] += 1
+    return type_sizes
+
+
+# ==============================
+
+
+df['type'] = None
+
+# repeat for each NTL class (cat_names)
+for c in cat_names:
+    cat_size = len(df.loc[df['label'] == c])
+    # example of alternative method (simpler, but does no ensure consistent class sizes)
+    # df.loc[df['label'] == c, 'type'] = np.random.choice(type_names, size=(cat_size,), p=type_weights)
+    type_sizes = gen_sample_size(cat_size, type_weights)
+    type_list = [[x] * type_sizes[i] for i,x in enumerate(type_names)]
+    type_list = list(itertools.chain.from_iterable(type_list))
+    np.random.shuffle(type_list)
+    df.loc[df['label'] == c, 'type'] = type_list
+
+
+
+
+
+for i in type_names:
+    tmp_df = df.loc[df['type'] == i]
+    print("Samples per cat ({}):".format(i))
+    for j in cat_names: print("{0}: {1}".format(j, sum(tmp_df['label'] == j)))
+
+
+
 # -----------------------------------------------------------------------------
 
+print("\nNormalizing class sizes...")
 
-# drop out some of the extra from low category
-original_df = df.copy(deep=True)
-
-# for debugging
-# df = original_df.copy(deep=True)
-
-
-
-print("Samples per cat (raw):")
-for i in cat_names: print("{0}: {1}".format(i, sum(df['label'] == i)))
-
-
-# ==============================
-
-
-# low_count = sum(df['label'] == 0)
-# other_count = sum(df['label'] == 1) + sum(df['label'] == 2)
-
-# keep_ratio = (other_count * 1.0) / low_count
-
-# keep_ratio = keep_ratio if keep_ratio < 1 else 1
-
-# df['drop'] = 'keep'
-# df.loc[df['label'] == 0, 'drop'] = np.random.choice(["drop", "keep"], size=(low_count,), p=[1-keep_ratio, keep_ratio])
-
-
-# ==============================
-# equal class sizes based on smallest class size
+# create equal class sizes based on smallest class size
+#   - randomizes which extras from larger classes are dropped
 
 raw_class_sizes = [sum(df['label'] == i) for i in cat_names]
 
@@ -217,59 +201,19 @@ for i in cat_names:
     keepers = np.random.permutation(class_index)[:nkeep]
     df.loc[keepers, 'drop'] = 'keep'
 
-
-# ==============================
-
-
 df = df.loc[df['drop'] == 'keep'].copy(deep=True)
+
+
+
+print("Samples per cat (raw):")
+for i in cat_names: print("{0}: {1}".format(i, sum(original_df['label'] == i)))
 
 print("Samples per cat (reduced):")
 for i in cat_names: print("{0}: {1}".format(i, sum(df['label'] == i)))
 
 
+
 # -----------------------------------------------------------------------------
-
-
-print("\nBuilding datasets...")
-
-
-# ==========
-# lsms_cluster['type'] = np.random.choice(["train", "val"], size=(len(lsms_cluster),), p=[0.90, 0.10])
-
-# train_df = lsms_cluster.loc[lsms_cluster['type'] == "train"]
-# val_df = lsms_cluster.loc[lsms_cluster['type'] == "val"]
-# ==========
-
-
-type_names = ["train", "val", "test", "predict"]
-type_cats = range(len(type_names))
-
-# ratio for train, val, test data
-# must sum to 1.0
-type_weights = [0.850, 0.150, 0.0, 0.0]
-
-type_sizes = np.zeros(len(type_names)).astype(int)
-
-# note: sizes are based on nkeep which is for a single NTL class label
-# subsequent steps to label data class (train, val, etc) must be repeated
-# for each NTL class
-for i in type_cats:
-    type_sizes[i] = int(np.floor(nkeep * type_weights[i]))
-
-# used floor for all counts, so total can be short by one
-for _ in range(nkeep-sum(type_sizes)):
-    type_sizes[np.random.randint(0, len(type_names))] += 1
-
-
-type_list = [[type_names[i]] * type_sizes[i] for i in type_cats]
-type_list = list(itertools.chain.from_iterable(type_list))
-
-df['type'] = None
-# repeat for each NTL class (cat_names)
-for i in cat_names:
-    np.random.shuffle(type_list)
-    df.loc[df['label'] == i, 'type'] = type_list
-
 
 
 dataframe_dict = {}
