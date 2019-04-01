f4d2b44f363a472e0546f20095d6a68ae0488c06
userz <sgoodman@aiddata.wm.edu>
2019-04-01 14:18:37 -0400

    Update push script for separate jobs

--- push.sh
+++ push.sh
@@ -1,17 +1,45 @@
 #!/bin/sh
 
-src=/home/userz/git/geo-cnn
-dst="/sciclone/aiddata10/REU/projects/mcc_tanzania"
+dir=$1
 
-cp $src/prepare_survey_data.py $dst
-cp $src/s1_jobscript $dst
-cp $src/data_prep.py $dst
-cp $src/create_grid.py $dst
-cp $src/runscript.py $dst
-cp $src/resnet.py $dst
-cp $src/vgg.py $dst
-cp $src/load_data.py $dst
-cp $src/load_survey_data.py $dst
-cp $src/main.py $dst
-cp $src/s2_jobscript $dst
-cp $src/second_stage_model.py $dst
+if [[ $dir == "" ]]; then
+    echo "Must specify job dir"
+    exit 1
+fi
+
+src=/home/userz/git/geo-cnn
+base="/sciclone/aiddata10/REU/projects/mcc_tanzania"
+
+dst=$base/$dir
+
+mkdir -p $dst
+
+push() {
+    cp $src/$1 $dst/$1
+}
+
+push prepare_survey_data.py
+push data_prep.py
+push create_grid.py
+push runscript.py
+push resnet.py
+push vgg.py
+push load_data.py
+push load_survey_data.py
+push main.py
+push second_stage_model.py
+
+cp s1_jobscript tmp_s1_jobscript
+cp s2_jobscript tmp_s2_jobscript
+
+echo "python /sciclone/aiddata10/REU/projects/mcc_tanzania/"${dir}"/main.py" >> s1_jobscript
+echo "mpirun --mca mpi_warn_on_fork 0 --map-by node -np 80 python-mpi /sciclone/aiddata10/REU/projects/mcc_tanzania/"${dir}"/second_stage_model.py" >> s2_jobscript
+
+push s1_jobscript
+push s2_jobscript
+
+cp tmp_s1_jobscript s1_jobscript
+cp tmp_s2_jobscript s2_jobscript
+
+rm tmp_s1_jobscript
+rm tmp_s2_jobscript
