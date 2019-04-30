6bf0efe203474240eeba353bd1f8f4cac403f211
userz <sgoodman@aiddata.wm.edu>
2019-04-30 13:31:34 -0400

    Update load survey data to class instead of autorun import

--- load_survey_data.py
+++ load_survey_data.py
@@ -10,78 +10,98 @@ import numpy as np
 from load_data import NTL_Reader
 
 
-base_path = "/sciclone/aiddata10/REU/projects/mcc_tanzania"
+class SurveyData():
+    """Load survey data based on static settings from settings json
+    """
 
-surveys = {}
+    def __init__(self, base_path, settings):
 
+        self.base_path = base_path
 
-# -----------------------------------------------------------------------------
+        self.ntl_calibrated = settings["ntl_calibrated"]
+        self.ntl_dim = settings["ntl_dim"]
+        self.ntl_year = settings["ntl_year"]
+        self.survey = settings["survey"]
 
+        self.ntl = NTL_Reader(calibrated=self.ntl_calibrated)
+        self.ntl.set_year(self.ntl_year)
 
-ntl_calibrated = False
-ntl_dim = 7
+        self.surveys = {}
 
-# ntl data
-ntl = NTL_Reader(calibrated=ntl_calibrated)
-
-
-# -----------------------------------------------------------------------------
-
-ntl.set_year(2010)
-
-lsms2010_field = 'cons'
-
-lsms2010_clusters_path = os.path.join(
-    base_path, "data/surveys/final/tanzania_2010_lsms_cluster.csv")
-
-lsms2010_cluster = pd.read_csv(lsms2010_clusters_path, quotechar='\"',
-                           na_values='', keep_default_na=False,
-                           encoding='utf-8')
-
-lsms2010_cluster['ntl'] = lsms2010_cluster.apply(
-    lambda z: ntl.value(z['lon'], z['lat'], ntl_dim=ntl_dim), axis=1)
+        self._lsms2010_cluster()
+        self._lsms2012_cluster()
+        self._dhs2010_cluster()
+        self._dhs2015_cluster()
 
 
-lsms2010_cluster["pred_yval"] = lsms2010_cluster[lsms2010_field]
+    def _lsms2010_cluster(self):
+        lsms2010_field = 'cons'
 
-surveys["lsms2010_cluster"] = lsms2010_cluster
+        lsms2010_clusters_path = os.path.join(
+            self.base_path, "data/surveys/final/tanzania_2010_lsms_cluster.csv")
 
-# -----------------------------------------------------------------------------
+        lsms2010_cluster = pd.read_csv(lsms2010_clusters_path, quotechar='\"',
+                                na_values='', keep_default_na=False,
+                                encoding='utf-8')
 
-ntl.set_year(2012)
+        lsms2010_cluster['ntl'] = lsms2010_cluster.apply(
+            lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
 
-lsms2012_field = 'cons'
 
-lsms2012_clusters_path = os.path.join(
-    base_path, "data/surveys/final/tanzania_2012_lsms_cluster.csv")
+        lsms2010_cluster["pred_yval"] = lsms2010_cluster[lsms2010_field]
 
-lsms2012_cluster = pd.read_csv(lsms2012_clusters_path, quotechar='\"',
-                           na_values='', keep_default_na=False,
-                           encoding='utf-8')
+        self.surveys["lsms2010_cluster"] = lsms2010_cluster
 
-lsms2012_cluster['ntl'] = lsms2012_cluster.apply(
-    lambda z: ntl.value(z['lon'], z['lat'], ntl_dim=ntl_dim), axis=1)
 
-lsms2012_cluster["pred_yval"] = lsms2012_cluster[lsms2012_field]
+    def _lsms2012_cluster(self):
+        lsms2012_field = 'cons'
 
-surveys["lsms2012_cluster"] = lsms2012_cluster
+        lsms2012_clusters_path = os.path.join(
+            self.base_path, "data/surveys/final/tanzania_2012_lsms_cluster.csv")
 
-# -----------------------------------------------------------------------------
+        lsms2012_cluster = pd.read_csv(lsms2012_clusters_path, quotechar='\"',
+                                na_values='', keep_default_na=False,
+                                encoding='utf-8')
 
-ntl.set_year(2010)
+        lsms2012_cluster['ntl'] = lsms2012_cluster.apply(
+            lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
 
-dhs2010_field = 'wealthscore'
+        lsms2012_cluster["pred_yval"] = lsms2012_cluster[lsms2012_field]
 
-dhs2010_clusters_path = os.path.join(
-    base_path, "data/surveys/final/tanzania_2010_dhs_cluster.csv")
+        self.surveys["lsms2012_cluster"] = lsms2012_cluster
 
-dhs2010_cluster = pd.read_csv(dhs2010_clusters_path, quotechar='\"',
-                          na_values='', keep_default_na=False,
-                          encoding='utf-8')
 
-dhs2010_cluster['ntl'] = dhs2010_cluster.apply(
-    lambda z: ntl.value(z['lon'], z['lat'], ntl_dim=ntl_dim), axis=1)
+    def _dhs2010_cluster(self):
+        dhs2010_field = 'wealthscore'
 
-dhs2010_cluster["pred_yval"] = dhs2010_cluster[dhs2010_field]
+        dhs2010_clusters_path = os.path.join(
+            self.base_path, "data/surveys/final/tanzania_2010_dhs_cluster.csv")
 
-surveys["dhs2010_cluster"] = dhs2010_cluster
+        dhs2010_cluster = pd.read_csv(dhs2010_clusters_path, quotechar='\"',
+                                na_values='', keep_default_na=False,
+                                encoding='utf-8')
+
+        dhs2010_cluster['ntl'] = dhs2010_cluster.apply(
+            lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
+
+        dhs2010_cluster["pred_yval"] = dhs2010_cluster[dhs2010_field]
+
+        self.surveys["dhs2010_cluster"] = dhs2010_cluster
+
+
+    def _dhs2015_cluster(self):
+        dhs2015_field = 'wealthscore'
+
+        dhs2015_clusters_path = os.path.join(
+            self.base_path, "data/surveys/final/tanzania_2015_dhs_cluster.csv")
+
+        dhs2015_cluster = pd.read_csv(dhs2015_clusters_path, quotechar='\"',
+                                na_values='', keep_default_na=False,
+                                encoding='utf-8')
+
+        dhs2015_cluster['ntl'] = dhs2015_cluster.apply(
+            lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
+
+        dhs2015_cluster["pred_yval"] = dhs2015_cluster[dhs2015_field]
+
+        self.surveys["dhs2015_cluster"] = dhs2015_cluster
