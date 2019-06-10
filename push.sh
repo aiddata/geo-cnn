56b91a396772336435d4330d9b15f688c1daed50
sgoodm <sgoodman@aiddata.wm.edu>
2019-06-10 10:31:54 -0400

    Update push script for subdir

--- push.sh
+++ push.sh
@@ -18,7 +18,10 @@ push() {
     cp $src/$1 $dst/$1
 }
 
-# push prepare_survey_data.py
+push utility/prepare_ghana_survey_data.py
+push utility/prepare_tanzania_survey_data.py
+push utility/estimate_ntl_classes.py
+
 push data_prep.py
 push create_grid.py
 push runscript.py
