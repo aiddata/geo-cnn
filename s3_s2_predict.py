9e94af451c91eafd3edad9bb9c4d1af613a502ea
sgoodm <sgoodman@aiddata.wm.edu>
2019-07-02 16:47:01 -0400

    Work on s3_s2_predict script (still fleshing structure out)

--- s3_s2_predict.py
+++ s3_s2_predict.py
@@ -8,10 +8,7 @@ import pandas as pd
 
 from settings_builder import Settings
 
-from model_prep import (pearson_r2, ModelHelper,
-                        run_cv, find_best_alpha, predict_inner_test_fold,
-                        scale_features, train, train_and_predict,
-                        run_models, run_tasks)
+from model_predict import run_models, run_tasks
 
 
 # *****************
@@ -33,16 +30,50 @@ predict_hash = s.build_hash(s.data[s.config["predict"]], nchar=7)
 # timestamp = datetime.datetime.fromtimestamp(int(time.time())).strftime(
 #     '%Y_%m_%d_%H_%M_%S')
 
-mh = ModelHelper(settings=s)
 
-regex_fname = "predict_*_{}_{}_{}.csv".format(
-    predict_hash, s.config["version"], s.config["predict_tag"])
-regex_str = os.path.join(base_path, "output", "s1_predict", regex_fname)
-regex_search = glob.glob(regex_str)
+s3_info = s.data["third_stage"]
+
+model_list =s3_info["predict"]["models"]
+
+model_tag = s.config["model_tag"]
+
+predict_settings = s.data[s.config["predict"]]
+predict_hash = s.build_hash(predict_settings, nchar=7)
+
+tasks = s.hashed_iter()
+
+for ix, (param_hash, params) in enumerate(tasks):
+
+
+    grid_predict_id = "{}_{}".format(s3_info["grid"]["boundary_id"], s3_info["predict"]["imagery_year"])
+
+    grid_id_string = "{}_{}_{}_{}".format(
+        param_hash, grid_predict_id, s.config["version"], s.config["predict_tag"]
+    )
+
+    grid_predict_path = os.path.join(base_path, "output/s3_s1_predict/predict_{}_{}_{}_{}.csv".format(grid_id_string))
+
+
+
+    train_predict_id = predict_hash
+
+    train_id_string = "{}_{}_{}_{}".format(
+        param_hash, train_predict_id, s.config["version"], s.config["predict_tag"]
+    )
+
+    for name in model_list:
+        joblib_path = os.path.join(base_path, "output/s2_models/models_{}_{}_{}.joblib".format(
+            name, train_id_string, model_tag))
+
+        # loaded_model = joblib.load(joblib_path)
+
+
+
+
+
 
 qlist = ["_".join(os.path.basename(i).split("_")[1:])[:-4] for i in regex_search]
-# qlist = ["7a118a3_2019_03_28_12_48_37"]
-# qlist = pd.read_csv(os.path.join(base_path, "cnn_results_merge_1.csv"))["id_string"].tolist()
-# print qlist
 
-run_tasks(tasks=qlist, func=run_models, args=mh, mode=mode)
+
+
+run_tasks(tasks=qlist, func=run_models, args=s, mode=mode)
