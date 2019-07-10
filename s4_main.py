53f2f38794c0b839ddf007765564f3dba0cf491c
sgoodm <sgoodman@aiddata.wm.edu>
2019-07-10 14:27:34 -0400

    Finish s4 surface point predict rasterization and test

--- s4_main.py
+++ s4_main.py
@@ -2,6 +2,11 @@
 
 import os
 
+import pandas as pd
+import numpy as np
+from affine import Affine
+import rasterio
+
 from settings_builder import Settings
 
 
@@ -11,6 +16,7 @@ json_path = "settings/settings_example.json"
 json_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), json_path)
 # *****************
 # *****************
+json_path = "settings/ghana_2008_dhs.json"
 
 
 s = Settings()
@@ -23,6 +29,7 @@ predict_hash = s.build_hash(predict_settings, nchar=7)
 
 s3_info = s.data["third_stage"]
 model_list = s3_info["predict"]["models"]
+input_list = s3_info["predict"]["inputs"]
 
 model_tag = s.config["model_tag"]
 surface_tag = s.config["surface_tag"]
@@ -33,9 +40,7 @@ tasks = s.hashed_iter()
 qlist = []
 
 for ix, (param_hash, params) in enumerate(tasks):
-
     for model_name in model_list:
-
         s3_s2_string = "_".join(str(i) for i in [
             model_name,
             param_hash,
@@ -46,7 +51,23 @@ for ix, (param_hash, params) in enumerate(tasks):
             s.config["predict_tag"],
             s.config["model_tag"]
         ])
-
         s3_s2_path = os.path.join(s.base_path, "output/s3_s2_predict/predict_{}.csv".format(s3_s2_string))
-
-        s4_path = os.path.join(s.base_path, "output/s4_surface/surface_{}_{}.csv".format(s3_s2_string, surface_tag))
+        for input_name in input_list:
+            s4_path = os.path.join(s.base_path, "output/s4_surface/surface_{}_{}_{}.tif".format(input_name, s3_s2_string, surface_tag))
+            df = pd.read_csv(s3_s2_path)
+            shape = (max(df.row), max(df.column))
+            blank = np.zeros(shape)
+            for i, row in df.iterrows():
+                blank[row.row-1, row.column-1] = row[input_name]
+            xmin, ymax = min(df.lon), max(df.lat)
+            pixel_size =  s3_info["surface"]["pixel_size"]
+            meta = {}
+            meta["transform"] = Affine(pixel_size, 0, xmin,
+                                       0, -pixel_size, ymax)
+            meta['height'] = shape[0]
+            meta['width'] = shape[1]
+            meta['driver'] = 'GTiff'
+            meta['count'] = 1
+            meta['dtype'] = str(blank.dtype)
+            with rasterio.open(s4_path, 'w', **meta) as result:
+                result.write(np.array([blank]))
