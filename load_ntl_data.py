e913f7cd7574b908a78314c0ecd22860259dc5d6
sgoodm <sgoodman@aiddata.wm.edu>
2019-08-09 12:24:06 -0400

    Update NTL_Reader class to work with either dmsp or viirs data

--- load_ntl_data.py
+++ load_ntl_data.py
@@ -8,10 +8,20 @@ class NTL_Reader():
     """ Read in NTL data
     """
 
-    def __init__(self, calibrated=False):
-        self.base = "/sciclone/aiddata10/REU/geo/data/rasters/dmsp_ntl/v4composites"
-        if calibrated:
-            self.base = "/sciclone/aiddata10/REU/geo/data/rasters/dmsp_ntl/v4composites_calibrated_201709"
+    def __init__(self, ntl_type="dmsp", calibrated=False):
+        self.ntl_type = ntl_type
+        if ntl_type == "dmsp":
+            self.base = "/sciclone/aiddata10/REU/geo/data/rasters/dmsp_ntl/v4composites"
+            if calibrated:
+                self.base = "/sciclone/aiddata10/REU/geo/data/rasters/dmsp_ntl/v4composites_calibrated_201709"
+            self.fregex = os.path.join(self.base, "F*{0}.*.tif")
+            self.default_dim = 7
+        elif ntl_type == "viirs":
+            self.base = "/sciclone/aiddata10/REU/geo/data/rasters/viirs/vcmcfg_dnb_composites_v10/yearly/max/"
+            self.fregex = os.path.join(self.base, "{0}_*.tif")
+            self.default_dim = 14
+        else:
+            raise ValueError("NTL_Reader: invalid ntl_type `{}`".format(ntl_type))
         self.year = None
         self.path = None
         self.file = None
@@ -20,12 +30,14 @@ class NTL_Reader():
         if self.file is not None:
             self.file.close()
         self.year = year
-        self.path = glob.glob(os.path.join(self.base, "*{0}*.tif".format(self.year)))[0]
+        self.path = glob.glob(self.fregex.format(self.year))[0]
         self.file = rasterio.open(self.path)
 
-    def value(self, lon, lat, ntl_dim=7, method="mean"):
+    def value(self, lon, lat, ntl_dim=-1, method="mean"):
         """Get nighttime lights average value for grid around point
         """
+        if ntl_dim == -1:
+            ntl_dim = self.default_dim
         r, c = self.file.index(lon, lat)
         ntl_win = ((r-ntl_dim/2+1, r+ntl_dim/2+1), (c-ntl_dim/2+1, c+ntl_dim/2+1))
         ntl_data = self.file.read(1, window=ntl_win)
