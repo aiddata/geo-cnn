8c75a57278beaf9fc257eef64b4c040494e2bfdf
sgoodm <sgoodman@aiddata.wm.edu>
2020-02-07 15:44:11 -0500

    Update to build surfaces from either s1 or s2 data

--- s4_main.py
+++ s4_main.py
@@ -1,6 +1,7 @@
 
 
 import os
+import itertools
 
 import pandas as pd
 import numpy as np
@@ -28,12 +29,9 @@ predict_hash = s.build_hash(predict_settings, nchar=7)
 
 
 s3_info = s.data["third_stage"]
-model_list = s3_info["predict"]["models"]
-input_list = s3_info["predict"]["inputs"]
 
 model_tag = s.config["model_tag"]
 surface_tag = s.config["surface_tag"]
-
 
 tasks = s.hashed_iter()
 
@@ -59,53 +57,97 @@ season_mosaics = rasterio.open(season_mosaics_path)
 
 print "-----"
 
+input_stage = s3_info["surface"]["input_stage"]
+
+if input_stage == "s2":
+    model_list = s3_info["predict"]["class_models"] + s3_info["predict"]["proba_models"]
+    input_list = s3_info["predict"]["inputs"]
+    surface_list = itertools.product(model_list, input_list)
+
+if input_stage == "s1":
+    surface_list = [
+        (0, s3_info["surface"]["value_type"])
+    ]
+
+else:
+    raise ValueError("Surface input stage must be either `s1` or `s2`. (`{}` given)".format(input_stage))
+
+
 for ix, (param_hash, params) in enumerate(tasks):
-    for model_name in model_list:
-        s3_s2_string = "_".join(str(i) for i in [
-            model_name,
-            param_hash,
-            predict_hash,
-            s3_info["grid"]["boundary_id"],
-            s3_info["predict"]["imagery_year"],
-            s.config["version"],
-            s.config["predict_tag"],
-            s.config["model_tag"]
-        ])
-        print "Running: {} - {}".format(param_hash, model_name)
-        s3_s2_path = os.path.join(s.base_path, "output/s3_s2_predict/predict_{}.csv".format(s3_s2_string))
-        for input_name in input_list:
-            s4_path = os.path.join(s.base_path, "output/s4_surface/surface_{}_{}_{}.tif".format(input_name, s3_s2_string, surface_tag))
-            df = pd.read_csv(s3_s2_path)
-            shape = (max(df.row), max(df.column))
-            blank = np.full(shape, s3_info["surface"]["nodata_val"])
 
-            for i, row in df.iterrows():
-                dim = s3_info["surface"]["dim"]
-                r, c = season_mosaics.index(row.lon, row.lat)
-                win = ((r-dim/2, r+dim/2), (c-dim/2, c+dim/2))
-                data = season_mosaics.read(1, window=win)
+    for surface_item in surface_list:
 
-                if data.shape != (dim, dim):
-                    raise Exception("bad feature (dim: ({0}, {0}), data shape: {1}".format(dim, data.shape))
+        print "Running: {} - {}".format(param_hash, surface_item)
 
-                val = row[input_name]
-                if np.sum(data == 0) / float(data.size) > s3_info["surface"]["scene_max_nodata"]:
-                    val = 0
+        if input_stage == "s2":
 
-                blank[row.row-1, row.column-1] = val
+            model_name, input_name = surface_item
+
+            input_string = "_".join(str(i) for i in [
+                model_name,
+                param_hash,
+                predict_hash,
+                s3_info["grid"]["boundary_id"],
+                s3_info["predict"]["imagery_year"],
+                s.config["version"],
+                s.config["predict_tag"],
+                s.config["model_tag"]
+            ])
+
+            input_path = os.path.join(s.base_path, "output/s3_s2_predict/predict_{}.csv".format(input_string))
 
 
-            xmin, ymax = min(df.lon), max(df.lat)
-            pixel_size =  s3_info["grid"]["pixel_size"]
-            meta = {}
-            meta["crs"] = rasterio.crs.CRS.from_epsg(4236)
-            meta["transform"] = Affine(pixel_size, 0, xmin,
-                                       0, -pixel_size, ymax)
-            meta['height'] = shape[0]
-            meta['width'] = shape[1]
-            meta['driver'] = 'GTiff'
-            meta['count'] = 1
-            meta['nodata'] = s3_info["surface"]["nodata_val"]
-            meta['dtype'] = str(blank.dtype)
-            with rasterio.open(s4_path, 'w', **meta) as result:
-                result.write(np.array([blank]))
+        elif input_stage == "s1":
+
+            _, input_name = surface_item
+
+            input_string = "_".join(str(i) for i in [
+                param_hash,
+                predict_hash,
+                s.config["version"],
+                s.config["predict_tag"]
+            ])
+
+            input_path = os.path.join(s.base_path, "output/s1_predict/predict_{}.csv".format(input_string))
+
+
+        surface_string = input_string + "_" + surface_tag
+        s3_surface_path = os.path.join(s.base_path, "output/s3_surface/surface_{}_{}.tif".format(input_name, surface_string))
+
+        df = pd.read_csv(input_path)
+        shape = (max(df.row), max(df.column))
+        blank = np.full(shape, s3_info["surface"]["nodata_val"])
+
+        for i, row in df.iterrows():
+            dim = s3_info["surface"]["dim"]
+            r, c = season_mosaics.index(row.lon, row.lat)
+            win = ((r-dim/2, r+dim/2), (c-dim/2, c+dim/2))
+            data = season_mosaics.read(1, window=win)
+
+            if data.shape != (dim, dim):
+                raise Exception("bad feature (dim: ({0}, {0}), data shape: {1}".format(dim, data.shape))
+
+            val = row[input_name]
+
+            # should this be checking if data == 0 or data == s3_info["surface"]["nodata_val"]?
+            # and should set value if true be 0 or nodataval?
+            if np.sum(data == 0) / float(data.size) > s3_info["surface"]["scene_max_nodata"]:
+                val = 0
+
+            blank[row.row-1, row.column-1] = val
+
+
+        xmin, ymax = min(df.lon), max(df.lat)
+        pixel_size =  s3_info["grid"]["pixel_size"]
+        meta = {}
+        meta["crs"] = rasterio.crs.CRS.from_epsg(4236)
+        meta["transform"] = Affine(pixel_size, 0, xmin,
+                                    0, -pixel_size, ymax)
+        meta['height'] = shape[0]
+        meta['width'] = shape[1]
+        meta['driver'] = 'GTiff'
+        meta['count'] = 1
+        meta['nodata'] = s3_info["surface"]["nodata_val"]
+        meta['dtype'] = str(blank.dtype)
+        with rasterio.open(s3_surface_path, 'w', **meta) as result:
+            result.write(np.array([blank]))
