c4f3e3ec065f871b7115fce02bced66bcf97a40b
sgoodm <sgoodman@aiddata.wm.edu>
2020-08-05 18:58:51 -0400

    Update load survey to make all survey funcs callable instead of auto run on init. Add survey id arg to acled func call

--- load_survey_data.py
+++ load_survey_data.py
@@ -14,11 +14,9 @@ class SurveyData():
     """Load survey data based on static settings from settings json
     """
 
-    def __init__(self, base_path, settings):
+    def __init__(self, base_path):
 
         self.base_path = base_path
-        self.survey = settings["survey"]
-
 
         # self.ntl_type = settings["ntl_type"]
         # self.ntl_calibrated = settings["ntl_calibrated"]
@@ -28,7 +26,15 @@ class SurveyData():
         # self.ntl = NTL_Reader(self.ntl_type, calibrated=self.ntl_calibrated)
         # self.ntl.set_year(self.ntl_year)
 
-        self.surveys = {}
+        self.surveys = {
+            "acled": _acled,
+            "tanzania_2010_lsms_cluster": _tanzania_2010_lsms_cluster,
+            "tanzania_2012_lsms_cluster": _tanzania_2012_lsms_cluster,
+            "tanzania_2010_dhs_cluster": _tanzania_2010_dhs_cluster,
+            "tanzania_2015_dhs_cluster": _tanzania_2015_dhs_cluster,
+            "ghana_2008_dhs_cluster": _ghana_2008_dhs_cluster,
+            "ghana_2014_dhs_cluster": _ghana_2014_dhs_cluster
+        }
 
         self._tanzania_2010_lsms_cluster()
         self._tanzania_2012_lsms_cluster()
@@ -36,7 +42,7 @@ class SurveyData():
         self._tanzania_2015_dhs_cluster()
         self._ghana_2008_dhs_cluster()
         self._ghana_2014_dhs_cluster()
-        self._acled()
+        # self._acled()
 
 
     def duplicate(self, df, mod):
@@ -62,20 +68,13 @@ class SurveyData():
         return new_df
 
 
-    def _acled(self):
+    def _acled(self, survey):
 
-        tmp_df_list = []
-        for i in self.survey:
-            # data_path = "/sciclone/aiddata10/REU/projects/lab_oi_nigeria/data/acled/final/acled_{}.csv".format(year)
-            data_path = self.base_path + "/data/acled/final/.csv".format(self.survey)
+        data_path = self.base_path + "/data/sample/{}.csv".format(survey)
 
-            tmp_data = pd.read_csv(data_path, quotechar='\"',
-                            na_values='', keep_default_na=False,
-                            encoding='utf-8')
-
-            tmp_df_list.append(tmp_data)
-
-        data = pd.concat(tmp_df_list)
+        data = pd.read_csv(data_path, quotechar='\"',
+                        na_values='', keep_default_na=False,
+                        encoding='utf-8')
 
         # binary indicating fatalities
         data["pred_yval"] = (data["fatalities"] > 0).astype(int)
@@ -84,14 +83,10 @@ class SurveyData():
         data["lon"] = data.longitude
         data["lat"] = data.latitude
 
-        # data['ntl'] = data.apply(
-        #     lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
-
-        # out_cols = ["lon", "lat", "pred_yval", "ntl"]
         out_cols = ["lon", "lat", "pred_yval"]
         data = data[out_cols]
-
-        self.surveys["acled_{}".format(year)] = data
+        # data['ntl'] = data.apply(
+        #     lambda z: self.ntl.value(z['lon'], z['lat'], ntl_dim=self.ntl_dim), axis=1)
 
 
     def _tanzania_2010_lsms_cluster(self):
